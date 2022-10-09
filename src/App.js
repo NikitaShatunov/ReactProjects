e996bce83d394173cd34a07b9342527db9bae3a9
Nikita <powerboy20042006@gmail.com>
2022-10-09 18:09:34 +0300

    list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
+import React from 'react';
 import './index.scss';
+import { Success } from './components/Success';
+import { Users } from './components/Users';
+
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+  const [search, setSearch] = React.useState('');
+  const [invites, setInvites] = React.useState([]);
+  const [success, setSuccess] = React.useState(false);
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users')
+    .then(result => result.json())
+    .then(json => {
+      setUsers(json.data)
+    }).catch(err => {
+      console.warn(err)
+      alert("Error!")
+    }).finally(() => setLoading(false))
+  }, [])
+
+  const onChangeSearch = (event) => {
+    setSearch(event.target.value)
+  }
+  const onClickInvite =(id) => {
+    if(invites.includes(id)){
+      setInvites(prev => prev.filter(_id => _id !== id));
+    } else {
+      setInvites(prev => [...prev, id])
+    }
+  }
+  const onClickSend = () => {
+    setSuccess(true);
+  }
   return (
     <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+      {
+        success ?  <Success count={invites.length}/> : <Users 
+        search = {search} 
+        items = {users} 
+        isLoading={isLoading}
+        onChangeSearch={onChangeSearch}
+        invites={invites}
+        onClickInvite={onClickInvite}
+        onClickSend={onClickSend}
+        />
+      
+      }
       </div>
-    </div>
   );
 }
 
